a56b50dc3f1bbd4fcc26927978f0dffd0e21bccc
BunchDevelopment <brandon_bunch@icloud.com>
2021-04-28 20:07:23 -0500

    remove some spaces

--- ch3.py
+++ ch3.py
@@ -1,25 +1,20 @@
 # Please debug these sections of code, until they work properly
 # Section One
-
 ans1 = input("Please enter one number: ")
 ans2 = input("Please enter a second number: ")
-
 an1 = int(ans1)
 an2 = int(ans2)
-
 sum1 = an1 + an2
 
 print(sum1)
 
 #Section Two
-
 a = 3
 b = 4.5
 
 print(float(a) + b)
 
 # Section Three
-
 c = 0
 v = 3.2
 if c == 0 or v == 0:
